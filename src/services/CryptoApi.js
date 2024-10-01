9382301373994cf2b4c848abb4715c8c24e02e27
Ehsan Atashkar <ehsanwebdev84@gmail.com>
2024-10-01 18:06:27 +0330

    Show coins in the table

--- src/services/CryptoApi.js
+++ src/services/CryptoApi.js
@@ -2,7 +2,7 @@ const BASE_URL = "https://api.coingecko.com/api/v3";
 const API_KEY = "CG-Ft4kKuBCANUzQNjBYBwiLQ9L";
 
 const getCoinList = () => {
-  return `${BASE_URL}/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=20&page=2&x_cg_demo_api_key=${API_KEY}`;
+  return `${BASE_URL}/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=20&page=1&x_cg_demo_api_key=${API_KEY}`;
 };
 
 export { getCoinList };
