b7c10c7109308811063c336ab1036f42ba5e9c46
Ehsan Atashkar <ehsanwebdev84@gmail.com>
2024-10-04 16:58:12 +0330

    Currency data is completed

--- src/components/template/HomePage.jsx
+++ src/components/template/HomePage.jsx
@@ -2,29 +2,31 @@ import { useEffect, useState } from "react";
 import TableCoin from "../module/Tablecoin";
 import { getCoinList } from "../../services/CryptoApi";
 import Pagination from "../module/Pagination";
+import Search from "../module/Search";
 
 function HomePage() {
   const [coins, setCoins] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
   const [page, setPage] = useState(1);
+  const [currency, setCurrency] = useState("usd");
 
   useEffect(() => {
     setIsLoading(true);
     const getData = async () => {
-      const res = await fetch(getCoinList(page));
+      const res = await fetch(getCoinList(page, currency));
       const json = await res.json();
       setCoins(json);
       setIsLoading(false);
     };
 
     getData();
-  }, [page]);
+  }, [page,currency]);
   {
     return (
       <div>
+        <Search currency={currency} setCurrency={setCurrency} />
         <TableCoin coins={coins} isLoading={isLoading} />
         <Pagination page={page} setPage={setPage} />
- 
       </div>
     );
   }
