ffc99536291daf42f32d61712678309dac69e919
Ehsan Atashkar <ehsanwebdev84@gmail.com>
2024-10-05 20:13:57 +0330

    add cleanup function

--- src/components/module/Search.jsx
+++ src/components/module/Search.jsx
@@ -6,15 +6,32 @@ function Search({ currency, setCurrency }) {
   const [coins, setCoins] = useState([]);
 
   useEffect(() => {
+    const controller = new AbortController();
+
     if (!text) return;
 
     const search = async () => {
-      const res = await fetch(searchCoin(text));
-      const json = await res.json();
-      if (json.coins) setCoins(json.coins);
+      try {
+        const res = await fetch(searchCoin(text), {
+          signal: controller.signal,
+        });
+        const json = await res.json();
+        console.log(json);
+        if (json.coins) {
+          setCoins(json.coins);
+        } else {
+          alert(json.status.error_message);
+        }
+      } catch (error) {
+        if (error.name !== "AbortError") {
+          alert(error.message);
+        }
+      }
     };
 
     search();
+
+    return () => controller.abort();
   }, [text]);
 
   return (
