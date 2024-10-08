3b719e9a3f701b98c3ff35f9b1368eff0dce7955
Ehsan Atashkar <ehsanwebdev84@gmail.com>
2024-10-08 21:54:06 +0330

    Draw a diagram

--- src/components/module/Chart.jsx
+++ src/components/module/Chart.jsx
@@ -1,5 +1,16 @@
 import { useState } from "react";
+
+import {
+  CartesianGrid,
+  Legend,
+  LineChart,
+  ResponsiveContainer,
+  Tooltip,
+  XAxis,
+  YAxis,
+} from "recharts";
 import { convertData } from "../../helpers/convertData";
+import { Line } from "recharts";
 
 import styles from "./Chart.module.css";
 
@@ -11,9 +22,33 @@ function Chart({ chart, setChart }) {
       <span className={styles.cross} onClick={() => setChart(null)}>
         X
       </span>
-      <div className={styles.chart}></div>
+      <div className={styles.chart}>
+        <div className={styles.graph}>
+          <ChartComponent data={convertData(chart, type)} type={type}/>
+        </div>
+      </div>
     </div>
   );
 }
 
 export default Chart;
+
+const ChartComponent = ({ data, type }) => {
+  return (
+    <ResponsiveContainer width="100%" height="100%">
+      <LineChart width={400} height={400} data={data}>
+        <Line
+          type="monotone"
+          dataKey={type}
+          stroke="#3874ff"
+          strokeWidth="2px"
+        />
+        <CartesianGrid stroke="#404042" />
+        <YAxis dataKey={type} domain={["auto", "auto"]} />
+        <XAxis dataKey="date" hide />
+        <Legend />
+        <Tooltip />
+      </LineChart>
+    </ResponsiveContainer>
+  );
+};
